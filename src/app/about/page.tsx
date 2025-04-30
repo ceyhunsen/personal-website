2016a13a02bb4778c1adb27eff4b6b374ba8c48e
Ceyhun Şen <ceyhuusen@gmail.com>
2025-04-30 23:04:00 +0300

    feat: Update about me page.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,27 +6,34 @@ export default function Index() {
       </header>
 
       <article>
+        <img src="/assets/about_me.jpg" />
         <p>
-          I am a computer engineer and like to work on low level softwares. I
-          hav experince in Linux driver development and bare metal applications
-          to this extent. I also like to work on high level softwares if the
-          project itself is interesting. My previous experiences are detailed in
-          my <a href="/ceyhun_sen_resume.pdf">resume</a>. I also occasionally
-          write <a href="technical-writings/">technical articles</a> about my
-          work.
+          I am a computer engineer by profession. I enjoy working on low-level
+          software. In college, I was part of a team and took responsibility of
+          developing low-level software for our projects. After college, I
+          worked on Linux device drivers and BSP (Board Support Package)
+          development. These fields are my favorite areas to work in and where
+          my expertise lies.
         </p>
 
         <p>
-          I like to driving and riding motorized vehicles, whether they are cars
-          or motorcycles. I like to go fast and go far. To feed my inner speed
-          demon, I play simulation games. On the other hand, I like to go slow
-          in real life and enjoy the scenery. If I discover a cool spot or a
-          route, I share it in my <a href="travel-logs/">travels logs</a>.
+          I also enjoy working on high-level software if the project is
+          interesting. Currently, I work in the blockchain space. My previous
+          experiences are detailed in my{" "}
+          <a href="/ceyhun_sen_resume.pdf">resume</a>.
         </p>
 
         <p>
-          I also share other miscellanous topics and thoughts on{" "}
-          <a href="blog/">my blog</a>.
+          I also share some of my hobbies here, in this website. I ride my
+          motorcycle and take pictures of the sceneries I see on the way. These
+          pictures and some of the cool routes I discover are shared in my{" "}
+          <a href="/travel-logs/">travel logs</a>.
+        </p>
+
+        <p>
+          I sometimes write <a href="/technical-articles">technical articles</a>{" "}
+          about my hobbies and work. I also share other miscellaneous topics and
+          thoughts on <a href="blog/">my blog</a>.
         </p>
       </article>
     </div>
