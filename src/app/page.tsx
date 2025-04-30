9458ef5f7e434d1d66ba681a658dde030dd76453
Ceyhun Şen <ceyhuusen@gmail.com>
2025-04-30 23:05:34 +0300

    feat: Update home page message.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
 
       <article>
         <p>
-          I am Ceyhun: A computer engineer, a motorcyclist and a traveller based
+          I am Ceyhun: A computer engineer, a motorcyclist and a traveler based
           in İstanbul/Türkiye.
         </p>
 
@@ -26,7 +26,9 @@ export default function Home() {
           knowledge/portfolio, travel experiences and other kinds of personal
           stuff. To know more about me, you can check out the{" "}
           <a href="/about">about me</a> page or{" "}
-          <a href="/ceyhun_sen_resume.pdf">my resume</a>.
+          <a href="/ceyhun_sen_resume.pdf">my resume</a>. You can also check out
+          my <a href="/blog">blog</a> for my thoughts and experiences, or{" "}
+          <a href="/travel-logs">travel logs</a> for my travel experiences.
         </p>
 
         <h2>Highlighted Posts</h2>
