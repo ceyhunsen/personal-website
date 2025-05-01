704c527d994f7c54d358825eeb17f63702a050ec
Ceyhun Şen <ceyhuusen@gmail.com>
2025-05-01 22:32:47 +0300

    feat: Add initial contact page.

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,79 @@
+import Link from "next/link";
+
+export default function Index() {
+  return (
+    <div>
+      <header>
+        <h1>Contact Me</h1>
+      </header>
+
+      <article>
+        <ul>
+          <li>
+            E-mail:{" "}
+            <a href="mailto:ceyhuusen@gmail.com" target="_blank">
+              ceyhuusen@gmail.com
+            </a>
+          </li>
+          <li>
+            LinkedIn:{" "}
+            <a href="https://www.linkedin.com/in/ceyhun-sen/" target="_blank">
+              linkedin.com/in/ceyhun-sen
+            </a>
+          </li>
+          <li>
+            Github:{" "}
+            <a href="https://github.com/ceyhunsen/" target="_blank">
+              github.com/ceyhunsen
+            </a>
+          </li>
+          <li>
+            Instagram:{" "}
+            <a href="https://www.instagram.com/ceyhusen/" target="_blank">
+              instagram.com/ceyhusen
+            </a>
+          </li>
+          <li>
+            X:{" "}
+            <a href="https://x.com/ceyhusen/" target="_blank">
+              x.com/ceyhusen
+            </a>
+          </li>
+          <li>
+            YouTube:{" "}
+            <a href="https://www.youtube.com/@ceyhu_sen/" target="_blank">
+              youtube.com/@ceyhu_sen
+            </a>
+          </li>
+          <li>
+            Reddit:{" "}
+            <a href="https://www.reddit.com/user/cleyclun/" target="_blank">
+              reddit.com/user/cleyclun
+            </a>
+          </li>
+          <li>
+            Spotify:{" "}
+            <a
+              href="https://open.spotify.com/user/mw9ifhrrbmj8sl3h98i7ndcp6?si=3057975b13744f20/"
+              target="_blank"
+            >
+              Ceyhun Şen
+            </a>
+          </li>
+          <li>
+            Steam:{" "}
+            <a href="https://steamcommunity.com/id/cleyclun/" target="_blank">
+              steamcommunity.com/id/cleyclun
+            </a>
+          </li>
+          <li>
+            Xbox:{" "}
+            <a href="https://www.xbox.com/play/user/cleyclun" target="_blank">
+              xbox.com/play/user/cleyclun
+            </a>
+          </li>
+        </ul>
+      </article>
+    </div>
+  );
+}
