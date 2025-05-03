7a35cea7394565ff8ccf7f43aa9597f1ceb09703
Ceyhun Şen <ceyhuusen@gmail.com>
2025-05-04 00:30:00 +0300

    fix: Make margin 0.

--- src/_components/image.tsx
+++ src/_components/image.tsx
@@ -3,6 +3,12 @@
  * @description Components for handling images.
  */
 
+/**
+ * @description A component that displays an image with a caption.
+ *
+ * @param {string} source - Source URL of the image.
+ * @param {string} caption - Caption (also alt) for the image.
+ */
 export default function ImageWithCaption({
   source,
   caption,
@@ -11,8 +17,12 @@ export default function ImageWithCaption({
   caption: string;
 }) {
   return (
-    <figure>
-      <img src={source} alt={caption} />
+    <figure style={{ width: "100%", textAlign: "center", margin: "0" }}>
+      <img
+        src={source}
+        alt={caption}
+        style={{ width: "100%", height: "auto" }}
+      />
       <figcaption>{caption}</figcaption>
     </figure>
   );
